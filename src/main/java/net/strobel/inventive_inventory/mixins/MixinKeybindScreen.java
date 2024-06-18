e2a3b61dae67a9e13e36a4178a81fc177921e27a
Strobel273 <moritz.strobel@outlook.de>
2024-06-18 11:26:04 +0200

    Compatibility changes

--- src/main/java/net/strobel/inventive_inventory/mixins/MixinKeybindScreen.java
+++ src/main/java/net/strobel/inventive_inventory/mixins/MixinKeybindScreen.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(KeybindsScreen.class)
 public abstract class MixinKeybindScreen {
-    @Inject(method = "initBody", at = @At("HEAD"))
+    @Inject(method = "init", at = @At("HEAD"))
     private void init(CallbackInfo ci) {
         ProfileHandler.initialize();
     }
